e987a626938436f99361e48506a78881c34262de
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2025-09-29 13:48:51 +0100

    added growth chart

--- components/analytics/CustomLabel.tsx
+++ components/analytics/CustomLabel.tsx
@@ -13,7 +13,7 @@ interface PieLabelProps {
 }
 
 const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, value, index,growth=false }: PieLabelProps) => {
-  console.log("Growth in label:", growth);
+  
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
